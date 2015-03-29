e78e7342c35bc9daa906d71715292a2c9d2c4d22
Edward Paulet <espaulet@gmail.com>
2015-03-29 12:36:39 -0700

    Make moves with clicks

--- js/reversi-view.js
+++ js/reversi-view.js
@@ -3,9 +3,11 @@ $.Reversi = function (el) {
   this.board = new Board();
   this.$board = this.$el.find('.reversi-board');
   this.$messages = this.$el.find('.reversi-messages');
+  this.turn = 'black';
 
   this.makeBoard();
-  this.render();
+
+  this.registerEvents();
 };
 
 $.Reversi.prototype.makeBoard = function () {
@@ -16,20 +18,56 @@ $.Reversi.prototype.makeBoard = function () {
     squaresHtml += '<div class="square" data-coords="[' + [row, col] + ']"></div>';
   }
   this.$board.html(squaresHtml);
+  this.$messages.html('Your turn, ' + this.turn + '!');
+
+  this.render();
 };
 
 $.Reversi.prototype.render = function () {
   var $squares = this.$board.children('.square');
-  $squares.removeClass('black').removeClass('white');
+  $squares.empty();
   var board = this.board;
 
   $squares.each(function () {
     var $square = $(this);
     var piece = board.getPiece($square.data('coords'));
     if (piece) {
-      $square.addClass(piece.color);
+      $piece = $('<div class="piece"></div>');
+      $piece.addClass(piece.color);
+      $square.html($piece);
+      $square.addClass('occupied');
     }
   });
+
+  this.$messages.html('Your turn, ' + this.turn + '!');
+};
+
+$.Reversi.prototype.registerEvents = function () {
+  this.$board.on('click', '.square', this.makeMove.bind(this));
+};
+
+$.Reversi.prototype.makeMove = function (event) {
+  var $square = $(event.currentTarget);
+  var pos = $square.data('coords');
+  var color = this.turn;
+
+  if (this.board.validMove(pos, color)) {
+    this.board.placePiece(pos, color);
+    if (this.board.isOver()) {
+      this.endGame();
+    }
+    this.switchTurn();
+    this.render();
+  }
+};
+
+$.Reversi.endGame = function () {
+  this.$board.off('click');
+  this.$messages.html('You win, ' + this.turn + '!');
+};
+
+$.Reversi.prototype.switchTurn = function () {
+  this.turn = this.turn === "black" ? "white" : "black";
 };
 
 $.fn.reversi = function () {
